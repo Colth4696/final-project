d97a57cab637e4b882553d9ebea12b23e6993499
Colth4696 <hafer0@gmail.com>
2021-12-12 19:44:55 -0700

    chore/file-restructure update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root '/logged_in'
+  root 'welcome#index'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
@@ -13,5 +13,4 @@ Rails.application.routes.draw do
 
   mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
 end
