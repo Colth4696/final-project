b8972206a4060aca6a75441c5bab63f2a6b710dc
Colth4696 <hafer0@gmail.com>
2021-12-27 20:47:23 -0700

    database.yml configuration

--- backend/app/assets/config/manifest.js
+++
@@ -1,2 +0,0 @@
-//= link_directory ../images
-//= link_directory ../stylesheets .css
--- backend/db/migrate/20210124175217_add_volunteer_ref_to_chatroom.rb
+++
@@ -1,5 +0,0 @@
-class AddVolunteerRefToChatroom < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :chatrooms, :volunteer, foreign_key: true
-  end
-end
