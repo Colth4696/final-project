c7e3b48477a4723b856abbe3407f30b2facd2aa7
Colth4696 <hafer0@gmail.com>
2022-01-28 09:55:03 -0700

    Add 'final-project/app/' from commit '589e1e37195a0d58594fd2f00e2553f2738b1c3f'
    git-subtree-dir: final-project/app
    git-subtree-mainline: 589e1e37195a0d58594fd2f00e2553f2738b1c3f
    git-subtree-split: 589e1e37195a0d58594fd2f00e2553f2738b1c3f

---
+++ final-project/app/src/constants/index.js
@@ -0,0 +1,6 @@
+export const API_ROOT = 'https://neighboraid.herokuapp.com';
+export const API_WS_ROOT = 'ws:/cable';
+export const HEADERS = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+};
