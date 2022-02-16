3a769d1726f5b2b013942ef4105c82c8419367dc
Colth4696 <hafer0@gmail.com>
2022-02-16 10:13:10 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
