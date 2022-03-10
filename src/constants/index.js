a492d06166857ec2e8ba79afbf47796bcc78d8ce
Colth4696 <hafer0@gmail.com>
2022-03-10 12:01:13 -0700

    gemfile

--- src/constants/index.js
+++
@@ -1,6 +0,0 @@
-export const API_ROOT = 'https://neighboraid.herokuapp.com';
-export const API_WS_ROOT = 'ws:/cable';
-export const HEADERS = {
-    'Content-Type': 'application/json',
-    Accept: 'application/json'
-};
