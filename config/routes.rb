46eba27237e75d941d4e99ce1405c26040401c61
Colth4696 <hafer0@gmail.com>
2022-03-12 20:52:35 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
-  get '*path', to: "application#fallback_index_html", constraints: -> (request) do !request.xhr? && request.format.html?
+  # get '*path', to: "application#fallback_index_html", constraints: -> (request) do !request.xhr? && request.format.html?
 
   resources :users, only: [:create, :show, :index]
   resources :requests
